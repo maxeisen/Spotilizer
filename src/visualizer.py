9ffa7ad953081b98a9d8a5595ccabf3e6819c6d8
Rasamps <16ras6@queensu.ca>
2019-12-07 17:33:22 -0500

    visualiiizzing

--- src/visualizer.py
+++ src/visualizer.py
@@ -4,7 +4,7 @@ import time
 
 def tstart(turtle):
     turtle.penup()
-    turtle.goto(1/2-pos[0]/2,pos[1]/2 -1/2)
+    turtle.goto(1/2 - pos[0]/2, 1/2)
     turtle.pendown()
 
 def modelSel(lLevel):
@@ -20,9 +20,9 @@ class Mandelbrot:
         self.__colormap = ["#FFFFFF","#000000"]
 
 class Spiralizer:
-    def __init__(self,colors,dur,winwidth):
-        self.__cm = colourChoice.getPalette() #Returns the colour palette for the current song.
-        self.__factor = song.length*(winwidth/song.length) #Assign the length of the song to a dur variable
+    def __init__(self,dur=120):#,colors,dur,winwidth):
+        self.__cm = ['red']#colourChoice.getPalette() #Returns the colour palette for the current song.
+        self.__factor = 120*(960/120)#song.length*(winwidth/song.length) #Assign the length of the song to a dur variable
         self.__seen = []
         self.__its = int(dur)
     def getIts(self):
@@ -38,9 +38,30 @@ class Spiralizer:
         return self.__cm
 
 def drawSpiral():
+    t = Turtle(visible=False)
+    t.speed(5)
+    tstart(t)
+    spr = Spiralizer()
+    c = 0
+    tfinish = time.time() + 120
+    while(time.time() < tfinish):
+        ss = tfinish - time.time()
+        print(ss)
+        bw = c - ss
+        if (bw > 0 and spr.notInSeen(bw)):
+            t.setheading(90)  # 90 degrees is pointing straight up
+            # 180 degrees means "draw a semicircle"
+            t.circle(2 * ss/2, 180)
+            c = bw
+            spr.addCurr(c)
+        else:
+            t.setheading(270)  # 270 degrees is straight down
+            t.circle(2 * ss/2, 180)
+            c += ss
+            spr.addCurr(c)
 
 win = Screen()
 win.setup(width=.75,height=.75)
 pos = [win.window_width(),win.window_height()]
-dummy = DrawSpiral(win)
+dummy = drawSpiral()
 win.mainloop()
