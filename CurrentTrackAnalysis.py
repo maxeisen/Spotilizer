dffe49237eade4149b4fa6214bed5aaa6b6c0602
maxeisen <17mje@queensu.ca>
2019-12-07 13:35:31 -0500

    reading all user playback data

--- CurrentTrackAnalysis.py
+++ CurrentTrackAnalysis.py
@@ -5,12 +5,12 @@ import authentication
 spotify = authentication.getSpotify()
 
 
-currSong = spotify.current_user_playing_track()
-# print(currSong)
-currSongID = currSong['item']['id']
-# print(currSongID)
+currentPlayback = spotify.current_playback()
+print(currentPlayback)
+currSong = currentPlayback['item']
+# print(currentPlayback)
 
-trackAnalysis = spotify.audio_analysis(currSongID)
+# trackAnalysis = spotify.audio_analysis(currSongID)
 
-print(spotify.track(currSongID)['name'])
+# print(spotify.track(currSongID)['name'])
 # print(results)
--- authentication.py
+++ authentication.py
@@ -8,11 +8,12 @@ import json.decoder
 
 #put your username here
 username = "0000000000"
+scope = 'user-modify-playback-state user-read-currently-playing user-read-playback-state'
 
 def getSpotify():
     SPOTIPY_CLIENT_ID='068c0eb38bd249cd82d1097fae9e0644'
     SPOTIPY_CLIENT_SECRET='070b7a269c9040aea67420066725096a'
     SPOTIPY_REDIRECT_URI = 'http://google.com/'
-    token = util.prompt_for_user_token(username,'user-read-currently-playing',client_id=SPOTIPY_CLIENT_ID,client_secret=SPOTIPY_CLIENT_SECRET,redirect_uri=SPOTIPY_REDIRECT_URI)
+    token = util.prompt_for_user_token(username, scope,client_id=SPOTIPY_CLIENT_ID,client_secret=SPOTIPY_CLIENT_SECRET,redirect_uri=SPOTIPY_REDIRECT_URI)
     spotify = spotipy.Spotify(auth=token)
     return (spotify)
