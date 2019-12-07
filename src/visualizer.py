7517817e3f7b2abe2748876807f566367dd9349c
Rasamps <16ras6@queensu.ca>
2019-12-07 18:48:35 -0500

    visualizer improvements

--- src/visualizer.py
+++ src/visualizer.py
@@ -1,6 +1,6 @@
 from turtle import *
 import time
-#from colourChoice import *
+import dataRetrieve
 
 def tstart(turtle):
     turtle.penup()
@@ -15,61 +15,91 @@ def modelSel(lLevel):
     else:
         return 'spiral'
 
-<<<<<<< HEAD
-class Mandelbrot:
-    def __init__(self):
-        self.__colormap = ["#FFFFFF","#000000"]
-=======
-
-
-win = Screen()
-win.setup(width=.75,height=.75)
-t = Turtle(visible= False)
->>>>>>> origin/master
-
 class Spiralizer:
-    def __init__(self,dur=120):#,colors,dur,winwidth):
+    def __init__(self,dur):
         self.__cm = ['red']#colourChoice.getPalette() #Returns the colour palette for the current song.
-        self.__factor = 120*(960/120)#song.length*(winwidth/song.length) #Assign the length of the song to a dur variable
+        self.__factor = dur*(960/dur)#song.length*(winwidth/song.length) #Assign the length of the song to a dur variable
         self.__seen = []
         self.__its = int(dur)
     def getIts(self):
         return self.__its
     def notInSeen(self,curr):
-        if not(curr in self.__seen):
-            return True
-        else:
+        if (curr in self.__seen):
             return False
+        else:
+            return True
     def addCurr(self,curr):
         self.__seen.append(curr)
     def getColor(self):
         return self.__cm
 
-def drawSpiral():
+def drawSpiral(dur):
     t = Turtle(visible=False)
-    t.speed(5)
+    t.speed(9)
     tstart(t)
-    spr = Spiralizer()
+    spr = Spiralizer(dur)
     c = 0
-    tfinish = time.time() + 120
-    while(time.time() < tfinish):
-        ss = tfinish - time.time()
-        print(ss)
+    c = 0
+    for ss in range(0,spr.getIts()):
         bw = c - ss
         if (bw > 0 and spr.notInSeen(bw)):
             t.setheading(90)  # 90 degrees is pointing straight up
             # 180 degrees means "draw a semicircle"
-            t.circle(2 * ss/2, 180)
+            t.circle(5 * ss/2, 180)
             c = bw
             spr.addCurr(c)
         else:
             t.setheading(270)  # 270 degrees is straight down
-            t.circle(2 * ss/2, 180)
+            t.circle(5 * ss/2, 180)
             c += ss
             spr.addCurr(c)
 
+def turtleConvert(x, y):  # converts from turtle pixels to the complex plane
+    return complex(x / 50, y / 50)
+
+class Mandelbrot:
+    def __init__(self, dur):
+        self.__dur = int(dur)
+        self.__colormap = ['black', 'white']
+        self.__cardinality = self.__dur
+
+    def computeCardinality(self, c):
+        z = 0
+        self.__cardinality = self.__dur
+
+        for i in range(self.__dur):
+            z = z * z + c
+
+            if abs(z) > 2:
+                self.__cardinality = i
+                return
+    def getColor(self):
+        if self.__cardinality == self.__dur:
+            return self.__colormap[0]
+        return self.__colormap[1]
+
+def mbrotDisplay(dur):
+    mandelbrot = Mandelbrot(dur)
+    t = Turtle(visible=False)
+    t.speed(0)
+    tstart(t)
+    tfinish = time.time() + dur
+    win.tracer(0)
+    while (time.time() < tfinish):
+        currtime = int(time.time())
+        for y in range(-151, 151):
+            mandelbrot.computeCardinality(turtleConvert(currtime, y))
+            t.color(mandelbrot.getColor())
+            t.goto(y, y)
+        t.up()
+        t.goto(currtime+1, 151)
+        t.down()
+        win.update()
+    win.tracer(1)
+
 win = Screen()
 win.setup(width=.75,height=.75)
+songProps = dataRetrieve.dataRetrieve()
 pos = [win.window_width(),win.window_height()]
-dummy = drawSpiral()
+dummy = drawSpiral(songProps["duration"])
 win.mainloop()
