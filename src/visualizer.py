88e1ef8a030e0584576b2ce058f80b3964e55605
Rasamps <16ras6@queensu.ca>
2019-12-07 16:55:28 -0500

    creating the spiral capability

--- src/visualizer.py
+++ src/visualizer.py
@@ -1,7 +1,8 @@
 from turtle import *
+import time
+#from colourChoice import *
 
-def tstart(turtle,swin):
-    pos = [swin.window_width(),swin.window_height()]
+def tstart(turtle):
     turtle.penup()
     turtle.goto(1/2-pos[0]/2,pos[1]/2 -1/2)
     turtle.pendown()
@@ -15,17 +16,31 @@ def modelSel(lLevel):
         return 'spiral'
 
 class Mandelbrot:
-    def __init__
+    def __init__(self):
+        self.__colormap = ["#FFFFFF","#000000"]
+
+class Spiralizer:
+    def __init__(self,colors,dur,winwidth):
+        self.__cm = colourChoice.getPalette() #Returns the colour palette for the current song.
+        self.__factor = song.length*(winwidth/song.length) #Assign the length of the song to a dur variable
+        self.__seen = []
+        self.__its = int(dur)
+    def getIts(self):
+        return self.__its
+    def notInSeen(self,curr):
+        if not(curr in self.__seen):
+            return True
+        else:
+            return False
+    def addCurr(self,curr):
+        self.__seen.append(curr)
+    def getColor(self):
+        return self.__cm
+
+def drawSpiral():
 
 win = Screen()
 win.setup(width=.75,height=.75)
-t = Turtle(visible= False)
-t2 = Turtle(visible= False)
-tstart(t,win)
-tstart(t2,win)
-t.showturtle()
-t2.showturtle()
-
-
-
+pos = [win.window_width(),win.window_height()]
+dummy = DrawSpiral(win)
 win.mainloop()
