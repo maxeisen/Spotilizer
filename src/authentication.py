570f10a8ff91cd544a52a8d6bc1e7b4b7d9e7ebc
maxeisen <17mje@queensu.ca>
2020-06-15 08:01:07 -0700

    secrets file added

--- src/authentication.py
+++ src/authentication.py
@@ -1,3 +1,4 @@
+import assets.secrets as secrets
 import spotipy
 import os
 import sys
@@ -10,8 +11,8 @@ username = "0000000000"
 scope = 'user-modify-playback-state user-read-playback-state user-read-currently-playing'
 
 def getSpotify():
-    SPOTIPY_CLIENT_ID='068c0eb38bd249cd82d1097fae9e0644'
-    SPOTIPY_CLIENT_SECRET='070b7a269c9040aea67420066725096a'
+    SPOTIPY_CLIENT_ID=secrets.SPOTIPY_CLIENT_ID
+    SPOTIPY_CLIENT_SECRET=secrets.SPOTIPY_CLIENT_SECRET
     SPOTIPY_REDIRECT_URI = 'http://google.com/'
     token = util.prompt_for_user_token(username, scope,client_id=SPOTIPY_CLIENT_ID,client_secret=SPOTIPY_CLIENT_SECRET,redirect_uri=SPOTIPY_REDIRECT_URI)
     spotify = spotipy.Spotify(auth=token)
